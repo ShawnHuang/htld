762bc732bae85101310046ed5dbc55b548cd2b09
shawnhuang <baxcky@gmail.com>
2016-05-01 22:58:30 +0800

    remove some redunt code

--- main/Main.cpp
+++ main/Main.cpp
@@ -28,7 +28,7 @@
 #include "ImAcq.h"
 #include "Gui.h"
 #include "TLDUtil.h"
-#include "Trajectory.h"
+//#include "Trajectory.h"
 
 using namespace tld;
 using namespace cv;
@@ -38,25 +38,12 @@ Main::Main()
 {
     tld = new tld::TLD();
     showOutput = 1;
-    printResults = NULL;
-    saveDir = ".";
     threshold = 0.5;
-    showForeground = 0;
 
-	showTrajectory = false;
 	trajectoryLength = 0;
-
-    selectManually = 0;
-
-    initialBB = NULL;
-    showNotConfident = true;
 
     reinit = 0;
 
-    loadModel = false;
-
-    exportModelAfterRun = false;
-    modelExportFile = "model";
     seed = 0;
 
     gui = NULL;
@@ -72,86 +59,53 @@ Main::~Main()
 
 void Main::doWork()
 {
-	Trajectory trajectory;
-    IplImage *img = imAcqGetImg(imAcq);
-    Mat grey(img->height, img->width, CV_8UC1);
-    cvtColor(cvarrToMat(img), grey, CV_BGR2GRAY);
+	//Trajectory trajectory;
 
-    tld->detectorCascade->imgWidth     = grey.cols;
-    tld->detectorCascade->imgHeight    = grey.rows;
-    tld->detectorCascade->imgWidthStep = grey.step;
-	tld->imBlurred                     = new cv::Mat(grey.rows, grey.cols, CV_8UC1);//To Allocate it Once...
-	tld->ppHolder                      = new cv::Mat(grey.rows, grey.cols, CV_8UC1);//To Allocate it Once...
-#ifdef USE_HTLD
-	//Initialize H-TLD...
-	tld->hTLDMaster                = createHETLDMasterModule(grey.cols, grey.rows, 2.0, true, true);
-	tld->detectorCascade->fastDet  = tld->hTLDMaster->getFastDet();
-	tld->medianFlowTracker->fastTr = tld->hTLDMaster->getFastTr();
-	tld->memMgr                    = tld->hTLDMaster->getMemModule();
-#endif
-	if(showTrajectory)
-	{
-		trajectory.init(trajectoryLength);
-	}
+    
 
-    if(selectManually)
-    {
-        CvRect box;
 
-        if(getBBFromUser(img, box, gui) == PROGRAM_EXIT)
-        {
-            return;
-        }
+    bool reuseFrameOnce = true;
 
-        if(initialBB == NULL)
-        {
-            initialBB = new int[4];
-        }
-
-        initialBB[0] = box.x;
-        initialBB[1] = box.y;
-        initialBB[2] = box.width;
-        initialBB[3] = box.height;
-    }
-
-    FILE *resultsFile = NULL;
-
-    if(printResults != NULL)
-    {
-        resultsFile = fopen(printResults, "w");
-        if(!resultsFile)
-        {
-            fprintf(stderr, "Error: Unable to create results-file \"%s\"\n", printResults);
-            exit(-1);
-        }
-    }
-
-    bool reuseFrameOnce = false;
-    bool skipProcessingOnce = false;
-
-    if(loadModel && modelPath != NULL)
-    {
-        tld->readFromFile(modelPath);
-        reuseFrameOnce = true;
-    }
-    else if(initialBB != NULL)
-    {
-        Rect bb = tldArrayToRect(initialBB);
-
-        printf("Starting at %d %d %d %d\n", bb.x, bb.y, bb.width, bb.height);
-
-        tld->selectObject(grey, &bb);
-        skipProcessingOnce = true;
-        reuseFrameOnce = true;
-    }
-	//dispatchOSToFile("deneme.txt");
+    IplImage *img;
     while(imAcqHasMoreFrames(imAcq))
     {
-       
-        if(!reuseFrameOnce)
+        img = imAcqGetImg(imAcq);
+        Mat grey(img->height, img->width, CV_8UC1);
+        cvtColor(cvarrToMat(img), grey, CV_BGR2GRAY);
+
+        if(reuseFrameOnce)
         {
-            cvReleaseImage(&img);
-            img = imAcqGetImg(imAcq);
+
+          tld->detectorCascade->imgWidth     = grey.cols;
+          tld->detectorCascade->imgHeight    = grey.rows;
+          tld->detectorCascade->imgWidthStep = grey.step;
+
+          tld->imBlurred                     = new cv::Mat(grey.rows, grey.cols, CV_8UC1);//To Allocate it Once...
+          tld->ppHolder                      = new cv::Mat(grey.rows, grey.cols, CV_8UC1);//To Allocate it Once...
+#ifdef USE_HTLD
+          //Initialize H-TLD...
+          tld->hTLDMaster                = createHETLDMasterModule(grey.cols, grey.rows, 2.0, true, true);
+          tld->detectorCascade->fastDet  = tld->hTLDMaster->getFastDet();
+          tld->medianFlowTracker->fastTr = tld->hTLDMaster->getFastTr();
+          tld->memMgr                    = tld->hTLDMaster->getMemModule();
+#endif
+
+            CvRect box;
+
+            if(getBBFromUser(img, box, gui) == PROGRAM_EXIT)
+            {
+                return;
+            }
+
+            Rect bb = Rect(box);
+
+            printf("Starting at %d %d %d %d\n", bb.x, bb.y, bb.width, bb.height);
+
+            tld->selectObject(grey, &bb);
+            reuseFrameOnce = false;
+        }
+        else
+        {
 
             if(img == NULL)
             {
@@ -159,50 +113,15 @@ void Main::doWork()
                 break;
             }
 
-            cvtColor(cvarrToMat(img), grey, CV_BGR2GRAY);
+            tld->processImage(cvarrToMat(img));
         }
 
-		double tic = cvGetTickCount();
-        if(!skipProcessingOnce)
-        {
-			tld->processImage(cvarrToMat(img));
-        }
-        else
-        {
-            skipProcessingOnce = false;
-        }
-		double toc = (cvGetTickCount() - tic) / cvGetTickFrequency();
 
-        if(printResults != NULL)
-        {
-            if(tld->currBB != NULL)
-            {
-                fprintf(resultsFile, "%.2d,%.2d,%.2d,%.2d,%f\n", tld->currBB->x, tld->currBB->y, tld->currBB->x + tld->currBB->width, tld->currBB->y + tld->currBB->height, tld->currConf);
-            }
-            else
-            {
-                fprintf(resultsFile, "NaN,NaN,NaN,NaN,NaN\n");
-            }
-        }
-		
-        toc = toc / 1000000;
-
-        float fps = 1 / toc;
-        
 		int confident = (tld->currConf >= threshold) ? 1 : 0;
 
-        if(showOutput || saveDir != NULL)
+        if(showOutput)
         {
-            char string[128];
 
-            char learningString[10] = "";
-
-            if(tld->learning)
-            {
-                strcpy(learningString, "Learning");
-            }
-
-            sprintf(string, "#%d,Posterior %.2f; fps: %.2f, #numwindows:%d, %s", imAcq->currentFrame - 1, tld->currConf, fps, tld->detectorCascade->numWindows, learningString);
             CvScalar yellow = CV_RGB(255, 255, 0);
             CvScalar blue = CV_RGB(0, 0, 255);
             CvScalar black = CV_RGB(0, 0, 0);
@@ -213,40 +132,7 @@ void Main::doWork()
                 CvScalar rectangleColor = (confident) ? blue : yellow;
                 cvRectangle(img, tld->currBB->tl(), tld->currBB->br(), rectangleColor, 8, 8, 0);
 
-				if(showTrajectory)
-				{
-					CvPoint center = cvPoint(tld->currBB->x+tld->currBB->width/2, tld->currBB->y+tld->currBB->height/2);
-					cvLine(img, cvPoint(center.x-2, center.y-2), cvPoint(center.x+2, center.y+2), rectangleColor, 2);
-					cvLine(img, cvPoint(center.x-2, center.y+2), cvPoint(center.x+2, center.y-2), rectangleColor, 2);
-					trajectory.addPoint(center, rectangleColor);
-				}
             }
-			else if(showTrajectory)
-			{
-				trajectory.addPoint(cvPoint(-1, -1), cvScalar(-1, -1, -1));
-			}
-
-			if(showTrajectory)
-			{
-				trajectory.drawTrajectory(img);
-			}
-
-            CvFont font;
-            cvInitFont(&font, CV_FONT_HERSHEY_SIMPLEX, .5, .5, 0, 1, 8);
-            cvRectangle(img, cvPoint(0, 0), cvPoint(img->width, 50), black, CV_FILLED, 8, 0);
-            cvPutText(img, string, cvPoint(25, 25), &font, white);
-
-            if(showForeground)
-            {
-
-                for(size_t i = 0; i < tld->detectorCascade->detectionResult->fgList->size(); i++)
-                {
-                    Rect r = tld->detectorCascade->detectionResult->fgList->at(i);
-                    cvRectangle(img, r.tl(), r.br(), white, 1);
-                }
-
-            }
-
 
             if(showOutput)
             {
@@ -255,50 +141,11 @@ void Main::doWork()
 
                 if(key == 'q') break;
 
-#ifndef USE_HTLD
-                if(key == 'b')
-                {
-
-                    ForegroundDetector *fg = tld->detectorCascade->foregroundDetector;
-
-                    if(fg->bgImg.empty())
-                    {
-                        fg->bgImg = grey.clone();
-                    }
-                    else
-                    {
-                        fg->bgImg.release();
-                    }
-                }
-#endif
                 if(key == 'c')
                 {
                     //clear everything
                     tld->release();
                 }
-
-                if(key == 'l')
-                {
-                    tld->learningEnabled = !tld->learningEnabled;
-                    printf("LearningEnabled: %d\n", tld->learningEnabled);
-                }
-
-                if(key == 'a')
-                {
-                    tld->alternating = !tld->alternating;
-                    printf("alternating: %d\n", tld->alternating);
-                }
-
-                if(key == 'e')
-                {
-                    tld->writeToFile(modelExportFile);
-                }
-
-                if(key == 'i')
-                {
-                    tld->readFromFile(modelPath);
-                }
-
                 if(key == 'r')
                 {
                     CvRect box;
@@ -314,33 +161,13 @@ void Main::doWork()
                 }
             }
 
-            if(saveDir != NULL)
-            {
-                char fileName[256];
-                sprintf(fileName, "%s/%.5d.png", saveDir, imAcq->currentFrame - 1);
-
-                cvSaveImage(fileName, img);
-            }
         }
 
-        if(reuseFrameOnce)
-        {
-            reuseFrameOnce = false;
-        }
+
+      cvReleaseImage(&img);
+      img = NULL;
     }//End of while-Loop...
 
-    cvReleaseImage(&img);
-    img = NULL;
-
-    if(exportModelAfterRun)
-    {
-        tld->writeToFile(modelExportFile);
-    }
-
-    if(resultsFile)
-    {
-        fclose(resultsFile);
-    }
 	//resetOutputStream();
 #ifdef USE_HTLD
 	//Destroy H-TLD...
